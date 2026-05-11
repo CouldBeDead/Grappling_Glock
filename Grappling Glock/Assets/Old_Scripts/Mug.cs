c98183ac5e0a67b3bb389f9594dd7ca5a3a075be
brycemurphy96-star <brycemurphy96@gmail.com>
2026-05-11 16:57:48 -0600

    Final tweaks from Bryce

--- Grappling Glock/Assets/Old_Scripts/Mug.cs
+++ Grappling Glock/Assets/Old_Scripts/Mug.cs
@@ -7,14 +7,15 @@ public class SceneChangerSpin : MonoBehaviour
     [SerializeField] private Transform playerCapsule;
 
     [Header("Scene Settings")]
-    [SerializeField] private string sceneToLoad = "WiningRoom";
+    [SerializeField] private string sceneToLoad = "";
 
     [Header("Spin Settings")]
     [SerializeField] private float rotationSpeed = 45f;
 
     private void Update()
     {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
     }
 
     private void OnCollisionEnter(Collision collision)
